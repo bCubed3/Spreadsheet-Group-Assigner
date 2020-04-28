6baecde3354485922ef5e75efb9de4cd06644349
bCubed3 <sf.bcubed@gmail.com>
2020-04-27 21:35:09 -0700

    Update assigment.py
    comments

--- assigment.py
+++ assigment.py
@@ -4,7 +4,7 @@ from csv_to_dict import get_data
 
 assignments_max = {
     "a": 1,
-    "b": 1,
+    "b": 2,
     "c": 1,
     "d": 1
 }
@@ -16,17 +16,30 @@ assignments = {
     "d": []
 }
 
+# import from csv
 selections = get_data("Raw_data/choices.csv")
 
+# array for people who did not fill out the form
+no_choices = []
+
+# assign everyone to their first choice
 for person, choices in zip(selections, selections.values()):
-    assignments[choices[0]].append(person)
+    if not choices:
+        no_choices.append(person)
+    else:
+        assignments[choices[0]].append(person)
 
 all_assigned = False
 iterations = 1
 
+# iterate until everyone is assigned and no group has more than the max it can be assigned
 while not all_assigned:
+    # array of people who are in excess of their group (order is probably based on the order in which they filled
+    # out the form)
     to_reassign = []
     all_assigned = True
+    # loop through all assignment and find those that have people in excess
+    # remove those people and put them in the "to_reassign" list
     for option, people in zip(assignments, assignments.values()):
         if len(people) > assignments_max[option]:
             # add in a random.shuffle(people) if a random selection is wanted
@@ -35,7 +48,7 @@ while not all_assigned:
             print(to_reassign)
             print(assignments)
             all_assigned = False
-    to_remove = []
+    # reassign people to their next choice. If none, assign them to the first empty group
     for person in to_reassign:
         if iterations + 1 > len(selections[person]):
             for option, assigned in zip(assignments, assignments.values()):
@@ -43,9 +56,16 @@ while not all_assigned:
                     assignments[option].append(person)
                     break
             else:
-                print(person)
+                print("There has been an error with {}".format(person))
         else:
             assignments[selections[person][iterations]].append(person)
     iterations += 1
+
+# assign people who did not fill out their choices
+for person in no_choices:
+    for assignment in assignments:
+        if len(assignments[assignment]) < assignments_max[assignment]:
+            assignments[assignment].append(person)
+            break
 
 print(assignments)
