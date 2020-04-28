28cba202c32d203912a2fb0cc166fcc8c7e68e84
bCubed3 <sf.bcubed@gmail.com>
2020-04-27 21:43:19 -0700

    Update csv_to_dict.py
    columns with data can now be changed

--- csv_to_dict.py
+++ csv_to_dict.py
@@ -1,8 +1,8 @@
-def get_data(filename="Raw_data/choices.csv"):
+def get_data(filename="Raw_data/choices.csv", id_column=1, choices_column=2):
     with open(filename) as file:
         data = list(file.read().split("\n"))
         data_dict = {}
         for index, answer in enumerate(data):
             data[index] = answer.split(", ")
-            data_dict[data[index][0]] = data[index][1:]
+            data_dict[data[index][id_column - 1]] = data[index][choices_column - 1:]
         return data_dict
